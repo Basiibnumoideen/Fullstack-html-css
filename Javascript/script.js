6f42498412eef0acaadc1f4f86600b95c54ec8b7
Basiibnumoideen <Basiibnumoideen@gmail.com>
2026-09-01 12:30:53 +0530

    changed

---
+++ Javascript/script.js
@@ -0,0 +1,54 @@
+let car = "Tesla";
+let year = 2024;
+console.log("Car:", car, "Year:", year);
+
+// variable declaration
+// let car = "Tesla";
+// let year = 2024;
+
+// 3 types of variable declaration
+// var
+// let
+// const
+var data=10;
+console.log(data)
+// var let and const differences
+// var is function-scoped and can be redeclared and updated
+// let is block-scoped and can be updated but not redeclared
+// const is block-scoped and cannot be updated or redeclared
+
+// data types in javascript
+// array data type
+let fruits = ["apple", "banana", "orange"];
+console.log("Fruits:", fruits);
+console.log("First fruit:", fruits[0]);
+// number data type
+let age = 25;
+let price = 19.99;
+console.log("Age:", age, "Price:", price);
+// string data type
+let name = "John Doe";
+let message = "Hello, World!";
+console.log("Name:", name, "Message:", message);
+// undefined data type
+let x;
+console.log("Value of x:", x);
+// null data type
+let y = null;
+console.log("Value of y:", y);
+//boolean data type
+// boolean data type can have only two values: true or false
+let isStudent = true;
+let isGraduated = false;
+console.log("Is student:", isStudent);
+console.log("Is graduated:", isGraduated);
+
+// object data type
+let d={
+    name:'basi',
+    class:'E86',
+    mob:8590882253,
+    isActive:true
+}
+console.log(d)
+console.log("Name:", d.name);
